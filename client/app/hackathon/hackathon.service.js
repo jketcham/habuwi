65195174aa5f3bdb2a25e4ac30374587f12f5101
Jack Ketcham <kcajketcham@gmail.com>
2014-11-02 06:11:19 -0500

    join/leave/get team for hackathon

--- client/app/hackathon/hackathon.service.js
+++ client/app/hackathon/hackathon.service.js
@@ -27,11 +27,29 @@ angular.module('habuwiApp')
     removeTeam: function(id, team) {
       return Restangular.one('hackathons', id).one('teams', team).remove();
     },
+    joinTeam: function(id, team, user) {
+      return Restangular.one('hackathons', id).one('teams', team).all('members').post(user);
+    },
+    leaveTeam: function(id, team, user) {
+      return Restangular.one('hackathons', id).one('teams', team).one('members', user).remove();
+    },
+    getTeam: function(id, team) {
+      return Restangular.one('teams', team).get();
+    },
     addParticipant: function(id, participant) {
       return Restangular.one('hackathons', id).all('participants').post(participant);
     },
     removeParticipant: function(id, participant) {
-      return Restangular.one('hackathons', id).one('participant', participant).remove();
+      return Restangular.one('hackathons', id).one('participants', participant).remove();
+    },
+    getParticipant: function(id, participant) {
+      return Restangular.one('hackathons', id).one('participants', participant).get();
+    },
+    addProspect: function(id, participant, user) {
+      return Restangular.one('hackathons', id).one('participants', participant).all('prospects').post(user);
+    },
+    removeProspect: function(id, participant, user) {
+      return Restangular.one('hackathons', id).one('participants', participant).one('prospects', user).remove();
     }
   }
 }]);
