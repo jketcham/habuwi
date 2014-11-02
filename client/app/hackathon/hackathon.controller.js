9a01a7d3a4b9807520562e25f69d9c4f1fb30d85
Jack Ketcham <kcajketcham@gmail.com>
2014-11-02 06:08:11 -0500

    Create team, add participant, etc functions

--- client/app/hackathon/hackathon.controller.js
+++ client/app/hackathon/hackathon.controller.js
@@ -7,24 +7,73 @@ angular.module('habuwiApp')
   });
 
 angular.module('habuwiApp')
-  .controller('HackathonPageCtrl', function ($scope, $http, $state, socket, Hackathons, User, Auth, hackathon) {
+  .controller('HackathonPageCtrl', function ($scope, $http, $state, socket, Hackathons, User, Auth, hackathon, user) {
     $scope.hackathon = hackathon;
-    $scope.hackathon.participants = Hackathons.getParticipants().$object;
-    $scope.hackathon.teams = Hackathons.getTeams().$object;
-    $scope.currentParticipant = Hackathons.getCurrentUser().$object;
-    $scope.currentUser = {};
-    if(Auth.isLoggedIn()) {
-      $scope.currentUser = User.get();
-    }
+    $scope.currentUser = user;
+    $scope.currentUser.isParticipant = false;
+    $scope.isLoggedIn = Auth.isLoggedIn();
+    $scope.hackathon.participants = Hackathons.getParticipants(hackathon._id).$object;
+    $scope.hackathon.teams = Hackathons.getTeams(hackathon._id).$object;
 
-    $scope.joinHackathon = function(data) {
-      Hackathons.addParticipant(hackathon._id, data).then(function(res) {
-        if(res && !res.error) {
-          $scope.hackathon.participating = true;
-          $scope.hackathon.btn.class = 'btn-success';
-          $scope.hackathon.btn.text = 'Participating';
-          $state.go('hackathons.page', { id: hackathon._id });
+    var user_hackathon = {};
+
+    // Check if the user is logged in and a participant in
+    // this hackathon
+    if(Auth.isLoggedIn()) {
+      user.hackathons.forEach(function(result) {
+        if(result.hackathon == hackathon._id) {
+          console.log(result);
+          user_hackathon = result._id;
+          $scope.currentUser.isParticipant = true;
+          $scope.currentUser.team = result.team;
+          $scope.currentUser.hackathon = result;
+          return;
         }
       });
     }
+
+    /**
+     * Function to allow user to join a hackathon
+     * @param  {Object} data User form submitted to join group
+     */
+    $scope.joinHackathon = function(data) {
+      Hackathons.addParticipant(hackathon._id, data).then(function(res) {
+        if(res && !res.error) {
+          console.log(res);
+          $scope.hackathon.participating = true;
+          $scope.hackathon.btn.class = 'btn-success';
+          $scope.hackathon.btn.text = 'Participating';
+          $scope.hackathon.participants.push(res);
+          $scope.currentUser.hackathon = {};
+          $scope.currentUser.hackathon.hackathon = hackathon;
+          $scope.currentUser.hackathon.prospects = [];
+          $state.go('hackathons.page', { id: hackathon._id }, { reload: true });
+        }
+      });
+    }
+
+    /**
+     * Function to allow user to create a team
+     * @param  {Object} data User form submitted with team info
+     */
+    $scope.createTeam = function(data) {
+      data.host = user._id;
+      data.members = [];
+      data.members.push(user._id);
+      data.hackathon = hackathon._id;
+      data.user_hackathon = user_hackathon;
+      Hackathons.addTeam(hackathon._id, data).then(function(res) {
+        if(res && !res.error) {
+          $scope.hackathon.teams.push(data);
+          $scope.currentUser.hackathon.team = data;
+          $state.go('hackathons.page.teams.profile', { id: hackathon._id, team_id: res._id });
+        }
+      });
+    }
+  });
+
+angular.module('habuwiApp')
+  .controller('HackathonTeamProfileCtrl', function ($scope, $http, socket, Hackathons, team) {
+    $scope.team = team;
+
   });
